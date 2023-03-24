78b8d13fd16cecd51a296d1dd7bebd225dd7f8e7
Gabe Underwood <Gabe.Underwood@vu.com>
2023-03-24 18:03:24 -0500

    I did this interesting

--- counter.py
+++ counter.py
@@ -3,6 +3,11 @@ def countEmail():
     name = input("Enter file:")
     if len(name) < 1 : name = "mbox-short.txt"
     handle = open(name)
+    filelist = [i.split() for i in handle if i.startswith('From ')]
+    emails = [i[1] for i in filelist]
+    mydic = {i:emails.count(i) for i in emails}
+    max_key = max(mydic, key=mydic.get)
+    print(max_key, mydic[max_key])
         
 
 ## if you want to test locally run > python counter.py
